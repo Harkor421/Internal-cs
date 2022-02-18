8d194ac780b36abf8d4fe9a89fa8664d20bedc84
Samir Gonzalez <sekesie2@gmail.com>
2022-02-17 21:48:47 -0500

    sd

--- MicroHub/User.cs
+++ MicroHub/User.cs
@@ -20,25 +20,22 @@ namespace MicroHub
         public User()
         {
             InitializeComponent();
-            loaddb();
-            dataGridView1.RowHeadersVisible = false;
-            dataGridView1.Columns[0].HeaderText = "ID"; //Setting the names for the columns in the DataGrid
-            dataGridView1.Columns[1].HeaderText = "Name";
-            dataGridView1.Columns[2].HeaderText = "Password";
-            dataGridView1.Columns[3].HeaderText = "Email";
-            dataGridView1.Columns[3].HeaderText = "Username";
 
             OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=DatabaseInternal1.accdb");//databaseconnection
             con.Open();
             //Query to count the amount of users who have the application downloaded. 
             OleDbCommand cmd = new OleDbCommand("SELECT COUNT (download_status) FROM Downloads WHERE download_status = 1", con);
             String userdown = cmd.ExecuteScalar().ToString();
+            
+            //Query to count the amount of users.
+            OleDbCommand cm = new OleDbCommand("select count (user_ID) from Users", con);
+            label4.Text = cm.ExecuteScalar().ToString();
             label5.Text = userdown;
 
 
         }
         //Subprocedure that belongs to the display of the database in the datagrid and bind any changes to the database.
-        private void loaddb()
+        private void loaddb(String search)
         {
             dataGridView1.DataSource = null;
             dataTable.Clear(); //Clears the datatable for refreshing any changes
@@ -46,7 +43,7 @@ namespace MicroHub
             con.Open();
             try
             {
-                ds = new OleDbDataAdapter("select user_ID,user_name,user_pass,user_email, username from Users", con); //Query to display the wanted columns in the DB
+                ds = new OleDbDataAdapter("SELECT user_ID, user_name, user_pass, user_email, username FROM Users WHERE User_ID LIKE '**' AND (user_email LIKE  '**' OR user_name LIKE '**' OR user_pass LIKE '**');", con); //Query to display the wanted columns in the DB
                 oleCommandBuilder = new OleDbCommandBuilder(ds);
                 ds.Fill(dataTable);
                 bindingSource = new BindingSource { DataSource = dataTable };// Binds any changes done in the datagrid with the database
@@ -63,10 +60,11 @@ namespace MicroHub
         //Subprodcedure that belongs to the button for saving any changes done in the datagrid.
         private void button4_Click(object sender, EventArgs e)
         {
+            String search = textBox1.Text;
             dataGridView1.EndEdit(); 
             ds.Update(dataTable); //Updates the information in the Database
             MessageBox.Show("Changes have been saved");
-            loaddb();
+            loaddb(search);
         }
 
 
@@ -113,12 +111,14 @@ namespace MicroHub
 
         private void button2_Click(object sender, EventArgs e)
         {
-
             String search = textBox1.Text;
-            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=DatabaseInternal1.accdb");
-            con.Open();
-
-
+            loaddb(search);
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Columns[0].HeaderText = "ID"; //Setting the names for the columns in the DataGrid
+            dataGridView1.Columns[1].HeaderText = "Name";
+            dataGridView1.Columns[2].HeaderText = "Password";
+            dataGridView1.Columns[3].HeaderText = "Email";
+            dataGridView1.Columns[3].HeaderText = "Username";
 
 
 
